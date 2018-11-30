6b4eaadf0637d1a06691c900bed28e356eed2b55
sky <skygangsta@hotmail.com>
2018-11-30 15:48:47 +0800

    Add create file logger store path

--- file_logger.go
+++ file_logger.go
@@ -1,29 +1,53 @@
+/* Copyright 2018 sky<skygangsta@hotmail.com>. All rights reserved.
+ *
+ * Licensed under the Apache License, version 2.0 (the "License").
+ * You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
 package logger
 
 import (
 	"errors"
 	"fmt"
+	"github.com/skygangsta/go-utils"
 	"os"
 	"time"
 )
 
 type FileLogger struct {
 	*LogWriter
-	File *os.File
+	writer *os.File
 }
 
 func NewFileLogger(level int, configFile string, perm uint32) (*FileLogger, error) {
 	var (
 		this = &FileLogger{}
 		err  error
+		p    = util.NewPath()
 	)
 
-	this.File, err = os.OpenFile(configFile, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
+	filePath, err := p.Dir(configFile)
+	if err != nil {
+		return nil, err
+	}
+
+	err = p.Create(filePath, 0755)
+	if err != nil {
+		return nil, err
+	}
+
+	this.writer, err = os.OpenFile(configFile, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
 	if err != nil {
 		return nil, errors.New(fmt.Sprintf("error: Open config file %v", err))
 	}
 
-	this.LogWriter = NewLogWriter(this.File, level)
+	this.LogWriter = NewLogWriter(this.writer, level)
 
 	return this, nil
 }
--- logger.go
+++ logger.go
@@ -1,3 +1,15 @@
+/* Copyright 2018 sky<skygangsta@hotmail.com>. All rights reserved.
+ *
+ * Licensed under the Apache License, version 2.0 (the "License").
+ * You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
 package logger
 
 import (
@@ -24,7 +36,6 @@ func InitLogger(configFile string) error {
 	var (
 		err    error
 		config *config.Config
-		//this       *Logger
 	)
 
 	this = &Logger{
@@ -159,7 +170,7 @@ func Fatal(message string) {
 	Fatalf("%s", message)
 }
 
-//ALL < TRACE < DEBUG < INFO < WARN < ERROR < FATAL < OFF
+// ALL < TRACE < DEBUG < INFO < WARN < ERROR < FATAL < OFF
 func convertLevelName(levelName string) int {
 	level := 0
 	switch strings.ToUpper(levelName) {
